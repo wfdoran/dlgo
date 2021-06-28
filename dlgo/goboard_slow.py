0ad23e1ca823a560fa66f68ac56e5767b388596d
William Doran <wfdoran4@yahoo.com>
2021-06-28 12:13:21 -0700

    Start of class Board() (page 35).

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -52,3 +52,44 @@ class GoString():
             self.libertites == other.liberties
 
     
+class Board():
+    def __init__(self, num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.grid = {}
+
+    def is_on_grid(self, point):
+        return 1 <= point.row <= self.num_rows and \
+               1 <= point.col <= self.num_cols
+
+    def get(self, point):
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string.Color
+
+    def get_go_string(self, point):
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string
+
+    def place_stone(self, player, point):
+        assert self.is_on_grid(piont)
+        assert self._grid.get(point) is None
+        adjacent_same_color = []
+        adjacent_opposite_color = []
+        liberties = []
+        for neighbor in point.neighbors():
+            if not self.is_on_grid(neighbor):
+                continue
+            neighbor_string = self._grid_get(neighbor)
+            if neighbor_string is None:
+                liberties.append(neighbor)
+            elif neighbor.color == player:
+                if neighbor_string not in adjacent_same_color:
+                    adjacent_same_color.append(neighbor_string)
+            else:
+                if neighbor_string not in adjacent_opposite_color:
+                    adjacent_opposite_color.append(neighbor_string)
+        new_string = GoString(player, [point], liberties)
