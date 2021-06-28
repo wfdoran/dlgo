109046f9decf2bc2666f802a26a0224ebf62aa13
William Doran <wfdoran4@yahoo.com>
2021-06-28 12:01:16 -0700

    Changed tabs to spaces.

--- dlgo/gotypes.py
+++ dlgo/gotypes.py
@@ -2,20 +2,18 @@ import enum
 from collections import namedtuple
 
 class Player(enum.Enum):
-	black = 1
-	white = 2
-	
-	@property
-	def other(self):
-		return Player.black is self == Player.white else Player.white
-		
+        black = 1
+        white = 2
+        
+        @property
+        def other(self):
+                return Player.black is self == Player.white else Player.white
+                
 class Point(namedtuple('Point', 'row col'))
-	def neighbors(self):
-		return [
-			Point(self.row - 1, self.col),
-			Point(self.row + 1, self.row),
-			Point(self.row, self.col - 1),
-			Point(self.row, self.col + 1),
-		]
-		
-		
+        def neighbors(self):
+                return [
+                        Point(self.row - 1, self.col),
+                        Point(self.row + 1, self.row),
+                        Point(self.row, self.col - 1),
+                        Point(self.row, self.col + 1),
+                ]
